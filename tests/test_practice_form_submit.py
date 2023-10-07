541863d23f14380187a9b7f1bcdeeef0f2edebd4
Daniil Zverev <svarhik348@gmail.com>
2023-10-08 00:44:56 +0300

    test

--- tests/test_practice_form_submit.py
+++ tests/test_practice_form_submit.py
@@ -3,11 +3,6 @@ from qa_guru_7_12.data import users
 from qa_guru_7_12.pages.registration_page import RegistrationPage
 
 
-@allure.tag('registration')
-@allure.label("owner", "AlterAyrol")
-@allure.epic('Тестирование вкладок в боковом меню')
-@allure.feature('Раздел Practice Form')
-@allure.story("Пользователь заполняет форму регистрации тестовыми данными.")
 def test_successful_student_registration_form(browser_open_url):
     registration_page = RegistrationPage()
 
