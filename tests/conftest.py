be46b76eda5119d1147bb395889aa11bdf639574
Daniil Zverev <svarhik348@gmail.com>
2023-10-08 00:39:50 +0300

    conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,13 @@
-import allure
 from selene import browser
 import pytest
 
 
-@allure.step("Настройка рзмера окна браузера")
 @pytest.fixture
 def browser_size():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
 
 
-@allure.step("Отрытие сайта")
 @pytest.fixture
 def browser_open_url(browser_size):
     browser.config.base_url = 'https://demoqa.com'
