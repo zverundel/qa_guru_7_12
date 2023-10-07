9463b683ca161af7835e97c19af20f4f4653c908
Daniil Zverev <svarhik348@gmail.com>
2023-10-08 00:07:12 +0300

    init commit

---
+++ tests/test_practice_form_submit.py
@@ -0,0 +1,14 @@
+from qa_guru_7_12.data import users
+from qa_guru_7_12.pages.registration_page import RegistrationPage
+
+
+def test_successful_student_registration_form(browser_open_url):
+    registration_page = RegistrationPage()
+
+    registration_page.open()
+
+    # WHEN
+    registration_page.register_user(users.student)
+
+    # THEN
+    registration_page.should_register_user(users.student)
