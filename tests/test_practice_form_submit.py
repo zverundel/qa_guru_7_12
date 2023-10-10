9a2ad4cad947c7350ffadf2f29ca9b608f7a1bd3
Daniil Zverev <svarhik348@gmail.com>
2023-10-10 23:58:59 +0300

    Изменения в тесте

--- tests/test_practice_form_submit.py
+++ tests/test_practice_form_submit.py
@@ -1,7 +1,15 @@
 from qa_guru_7_12.data import users
 from qa_guru_7_12.pages.registration_page import RegistrationPage
+import allure
+from allure_commons.types import Severity
 
 
+@allure.tag('Web')
+@allure.severity(Severity.CRITICAL)
+@allure.label('owner', 'Evgenii Li')
+@allure.feature('Registration form')
+@allure.story('Assert registered user')
+@allure.link('https://demoqa.com/automation-practice-form')
 def test_successful_student_registration_form():
     registration_page = RegistrationPage()
 
